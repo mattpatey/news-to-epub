a9da496b17ba46fc3b7b42a6c9e5e5e119d33033
Matt Patey <matt.patey@ableton.com>
2018-08-20 22:23:22 +0200

    Refactor

--- src/news_to_epub/scrape.py
+++ src/news_to_epub/scrape.py
@@ -23,6 +23,8 @@ import sys
 from ebooklib import epub
 import requests
 
+from www_guardian_com import SourceWwwGuardianCom
+
 
 logger = logging.getLogger()
 
@@ -46,7 +48,7 @@ def make_ebook(title, contents):
             safe_title = safe_filename(a['title'])
             file_name = u'chapter-{}.xhtml'.format(safe_title)
             chapter = epub.EpubHtml(title=a['title'], file_name=file_name, lang='en')
-            chapter.content = u'<h1>{}</h1><h6>{}</h6>{}'.format(a['title'], a['date'], a['content'])
+            chapter.content = u'<h1>{}</h1><h6>{}</h6>{}<span epub:type="pagebreak"></span>'.format(a['title'], a['date'], a['content'])
             section_chapters.append(chapter)
             book.add_item(chapter)
         sections.append((name, section_chapters))
@@ -58,23 +60,15 @@ def make_ebook(title, contents):
 
     return book
 
-def main():
-    parser = argparse.ArgumentParser("Transform news from the web into an epub file.")
-    parser.add_argument('--loglevel', type=str, default='warn', help='Log level. Valid values include: debug, error, warn, info. Default: warn.')
-    parser.add_argument('--output-path', type=str, default='~', help='Path to where the .epub file will be written, e. g. ~/Desktop')
-    args = parser.parse_args()
-
-    if args.loglevel == 'debug':
+def configure_logging(level=logging.WARN):
+    if level == 'debug':
         level = logging.DEBUG
-    elif args.loglevel == 'error':
+    elif level == 'error':
         level = logging.ERROR
-    elif args.loglevel == 'warn':
+    elif level == 'warn':
         level = logging.WARN
-    elif args.loglevel == 'info':
+    elif level == 'info':
         level = logging.INFO
-    else:
-        print(u"Invalid log level '{}'. Using default value.".format(args.loglevel))
-        level = logging.WARN
 
     logger.setLevel(level)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -82,53 +76,52 @@ def main():
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
+def configure_news2epub():
     config_filepath = os.path.expanduser('~/news_to_epub.cfg')
+    config = SafeConfigParser()
     try:
-        config = SafeConfigParser()
         with open(config_filepath, 'r') as c:
             config.readfp(c)
     except IOError:
         msg = "Couldn't find configuration file {}".format(config_filepath)
-        logger.error(msg)
-        sys.exit(1)
+        raise
 
+    return config
+
+def fetch_content(sources, pickle_file):
     # Fetch articles from news source(s)
     #
     articles = defaultdict(list)
-    pickle_file = os.path.expanduser('~/news_to_epub.pkl')
 
     try:
         published_articles = get_published_articles(pickle_file)
     except IOError:
         published_articles = []
 
-    # TODO: Make use of a lightweight plug-in architecture to
-    # automatically pick-up modules for sourcing content from the web.
-    #
-    from www_guardian_com import get_content
-    for article in get_content(config.items('www_guardian_com')):
-        if get_article_hash('www_guardian_com', article) not in published_articles:
-            articles['www_guardian_com'].append(article)
-        else:
-            msg = u'Skipping already published article "{}".'.format(article['title'])
-            logger.warn(msg)
+    for source in sources:
+        # get_content should return a dict with the keys `title`,
+        # `date`, and `content`
+        for article in source.get_content():
+            if get_article_hash(source.name, article) not in published_articles:
+                articles[source.name].append(article)
+            else:
+                msg = u'Skipping already published article "{}".'.format(article['title'])
+                logger.warn(msg)
 
-    if sum(map(len, articles.values())) == 0:
-        logger.warn(u'No articles to publish. Exiting.')
-        sys.exit(0)
+    return articles
 
-    # Generate the .epub file for reader devices
-    #
+def create_epub_file(articles, output_path):
     date = datetime.now().strftime(u'%A %d %B %Y')
     title = u'Reading for {}'.format(date)
     book = make_ebook(title, articles)
     filename = '{}.epub'.format(safe_filename(title))
-    path = os.path.expanduser(args.output_path)
+    path = os.path.expanduser(output_path)
     filepath = os.path.join(path, filename)
     epub.write_epub(filepath, book, {})
 
-    # Update pickle with hashes of articles we just published.
-    #
+    return filepath
+
+def update_article_archive(articles, pickle_file):
     new_hashes = []
     for source, source_articles in articles.items():
         for article in source_articles:
@@ -157,6 +150,23 @@ def get_published_articles(pickle_file):
         published_hashes = upickler.load()
 
     return published_hashes
+
+
+def main():
+    parser = argparse.ArgumentParser("Transform news from the web into an epub file.")
+    parser.add_argument('--loglevel', type=str, default='warn', help='Log level. Valid values include: debug, error, warn, info. Default: warn.')
+    parser.add_argument('--output-path', type=str, default='~', help='Path to where the .epub file will be written, e. g. ~/Desktop')
+    args = parser.parse_args()
+
+    configure_logging(args.loglevel)
+    config = configure_news2epub()
+
+    archive = os.path.expanduser('~/news_to_epub.pkl')
+    content = fetch_content([
+        SourceWwwGuardianCom(config.get('www_guardian_com', 'api_key')),
+    ], archive)
+    create_epub_file(content, args.output_path)
+    update_article_archive(content, archive)
 
 if __name__ == '__main__':
     main()
--- src/news_to_epub/www_guardian_com.py
+++ src/news_to_epub/www_guardian_com.py
@@ -10,56 +10,68 @@ import requests
 
 logger = logging.getLogger()
 
-def search(api_args, **kwargs):
-    merged_args = dict(api_args, **kwargs)
-    logger.debug('Performing Guardian API search')
-    res = requests.get('http://content.guardianapis.com/search', params=merged_args)
-    res.raise_for_status()
-    logger.debug('Converting JSON')
-    return loads(res.text)['response']
 
-def all_pages(search_func, articles_per_page=10, max_pages=100):
-    res = search_func(page=1, pageSize=1)
-    for i in xrange(1, min(res['pages'] + 1, max_pages)):
-        logger.debug('Yielding page {}'.format(i))
-        try:
-            yield search_func(page=i, pageSize=articles_per_page)
-        except requests.exceptions.HTTPError, e:
-            error_msg = loads(e.res.text)['response']['message']
-            msg = '{}: {}'.format(error_msg, e.request.url)
-            logger.error(msg)
-            raise
+class SourceWwwGuardianCom(object):
 
-def excluded_article(article):
-    # Skip some content based on certain URL patterns
-    return any(s in article['id'] for s in ['observer-sudoku', '/crosswords/', '/live/'])
+    def __init__(self, api_key):
+        self.name = 'www_guardian_com'
+        self.api_key = api_key
 
-def collate(search_func):
-    articles = []
+    def _search(self, api_args, **kwargs):
+        hardcoded_args = { 'show-blocks': 'body' }
+        api_args.update(hardcoded_args)
+        merged_args = dict(api_args, **kwargs)
+        logger.debug('Performing Guardian API search')
+        res = requests.get('http://content.guardianapis.com/search', params=merged_args)
+        res.raise_for_status()
+        logger.debug('Converting JSON')
+        return loads(res.text)['response']
 
-    for page in all_pages(search_func, max_pages=2):
-        for article in page['results']:
-            if excluded_article(article):
-                logger.warn('Skipping article "{}".'.format(article['webUrl']))
-                continue
-            else:
-                title = article['webTitle']
-                date = datetime.strptime(article['webPublicationDate'], "%Y-%m-%dT%H:%M:%SZ")
-                sectionId = article['sectionId']
-                try:
-                    content = article['blocks']['body'][0]['bodyHtml']
-                except IndexError:
-                    logger.error('Failed to extract text from article {}'.format(article['webTitle']))
+    def _all_pages(self, search_func, articles_per_page=10, max_pages=100):
+        res = search_func(page=1, pageSize=1)
+        for i in xrange(1, min(res['pages'] + 1, max_pages)):
+            logger.debug('Yielding page {}'.format(i))
+            try:
+                yield search_func(page=i, pageSize=articles_per_page)
+            except requests.exceptions.HTTPError, e:
+                error_msg = loads(e.res.text)['response']['message']
+                msg = '{}: {}'.format(error_msg, e.request.url)
+                logger.error(msg)
+                raise
+
+    def _excluded_article(self, article):
+        # Skip some content based on certain URL patterns
+        return any(s in article['id'] for s in ['observer-sudoku', '/crosswords/', '/live/'])
+
+    def _collate(self, search_func):
+        articles = []
+
+        for page in self._all_pages(search_func, max_pages=10):
+            for article in page['results']:
+                if self._excluded_article(article):
+                    logger.warn('Skipping article "{}".'.format(article['webUrl']))
                     continue
-                articles.append(dict(title=title, date=date, content=content, section=sectionId))
+                else:
+                    title = article['webTitle']
+                    date = datetime.strptime(article['webPublicationDate'], "%Y-%m-%dT%H:%M:%SZ")
+                    try:
+                        content = article['blocks']['body'][0]['bodyHtml']
+                    except IndexError:
+                        logger.error('Failed to extract text from article {}'.format(article['webTitle']))
+                        continue
+                    articles.append(dict(title=title, date=date, content=content))
 
-    return articles
+        return articles
 
-def get_content(config):
-    # TODO: Document valid structure and content for `config`
-    search_args = dict(config)
+    def get_content(self):
+        args = {
+            'api-key':     self.api_key,
+            'from-date':   datetime.now().strftime('%Y-%m-%d'),
+            'order-by':    'newest',
+            'show-blocks': 'body',
+            'to-date':     datetime.now().strftime('%Y-%m-%d'),
+        }
+        search_func = partial(self._search, args)
+        articles = self._collate(search_func)
 
-    search_func = partial(search, search_args)
-    articles = collate(search_func)
-
-    return articles
+        return articles
